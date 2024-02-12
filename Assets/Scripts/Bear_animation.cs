a4d0cff800ff63e620fc1aef79995d1ebc639695
InfernalKangaroo <trane526@mail.ru>
2024-02-12 12:55:51 +0300

    Added project to GIT

---
+++ Assets/Scripts/Bear_animation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear_animation : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
